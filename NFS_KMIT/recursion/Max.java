9631ce952fb507f8a5351b9b1c6d33d3874d4a39
“aravindadepu23” <adepuaravind528@gmail.com>
2025-02-24 20:09:06 +0530

    new one

--- NFS_KMIT/recursion/Max.java
+++ NFS_KMIT/recursion/Max.java
@@ -1,14 +1,14 @@
 class Max {
     public static void main(String[] args){
-    int a[] = {1,2,3,5,1,3};
-       System.out.println(recursion(a,Integer.MIN_VALUE,0));
+    int b[] = {1,2,3,5,1,3};
+       System.out.println(recursion(b,Integer.MIN_VALUE,0));
     }
-    static int recursion(int a[],int max,int n){
-            if(n == a.length){
+    static int recursion(int b[],int max,int n){
+            if(n == b.length){
                 return max;
             }
-            if(max<a[n]){max = a[n];}
-            return recursion(a,max,n+1);
+            if(max<b[n]){max = b[n];}
+            return recursion(b,max,n+1);
         
     }
 }
